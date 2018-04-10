01bbd526897f4f0f012b288844cef9db05596550
tivua099pvt@gmail.com <luannguyen23081996@gmail.com>
2018-04-11 00:08:22 +0700

    frist

---
+++ QLBX/ViewModel/KhachHangVM.cs
@@ -0,0 +1,41 @@
+﻿using QLBX.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QLBX.ViewModel
+{
+    public class KhachHangVM
+    {
+        private List<KHACHHANG> _KhachHangList;
+
+        public List<KHACHHANG> KhachHangList
+        {
+            get { return _KhachHangList; }
+            set { _KhachHangList= value; }
+        }
+
+        QLBXEntities DB = new QLBXEntities();
+
+
+        public KhachHangVM()
+        {
+            var khachhang = DB.KHACHHANGs.ToList();
+            KhachHangList = khachhang;
+
+        //DeleteCommand = new RelayCommand<object>((p) => p != null, (p) => {
+        //    Listf.Remove(p as SINHVIEN);
+        //});
+    }
+
+    //public ICommand DeleteCommand { get; set; }
+
+    //public ICommand Command { get; set; }
+
+
+}
+}
